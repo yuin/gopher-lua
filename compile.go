0d66873a9eed4868438c4a1e86a8197ac20f84c8
Lion Yang <lion@aosc.xyz>
2017-02-02 10:58:32 +0800

    compile.go: fix typo

--- compile.go
+++ compile.go
@@ -1089,7 +1089,7 @@ func constFold(exp ast.Expr) ast.Expr { // {{{
 			case "^":
 				return &constLValueExpr{Value: LNumber(math.Pow(float64(lvalue), float64(rvalue)))}
 			default:
-				panic(fmt.Sprintf("unknwon binop: %v", expr.Operator))
+				panic(fmt.Sprintf("unknown binop: %v", expr.Operator))
 			}
 		} else {
 			retexpr := *expr
