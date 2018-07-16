10450a1e8e5cfbcd9e1ff63c6ed086e4bedc0a65
Mark Tully <mark.tully@lockwood-publishing.com>
2018-07-16 23:31:42 +0100

    LNumber2I optimisations
    remove unused slice from each allocator page
      (saves ~512 bytes per page)
    stop temp value escaping to heap and creating garbage

--- alloc.go
+++ alloc.go
@@ -26,52 +26,58 @@ func init() {
 
 // allocator is a fast bulk memory allocator for the LValue.
 type allocator struct {
-	top         int
-	size        int
-	nptrs       []LValue
-	nheader     *reflect.SliceHeader
-	fptrs       []float64
-	fheader     *reflect.SliceHeader
-	itabLNumber unsafe.Pointer
+	top     int
+	size    int
+	fptrs   []float64
+	fheader *reflect.SliceHeader
+
+	scratchValue  LValue
+	scratchValueP *iface
 }
 
 func newAllocator(size int) *allocator {
 	al := &allocator{
-		top:         0,
-		size:        size,
-		nptrs:       make([]LValue, size),
-		nheader:     nil,
-		fptrs:       make([]float64, size),
-		fheader:     nil,
-		itabLNumber: unsafe.Pointer(nil),
+		top:     0,
+		size:    size,
+		fptrs:   make([]float64, size),
+		fheader: nil,
 	}
-	al.nheader = (*reflect.SliceHeader)(unsafe.Pointer(&al.nptrs))
 	al.fheader = (*reflect.SliceHeader)(unsafe.Pointer(&al.fptrs))
+	al.scratchValue = LNumber(0)
+	al.scratchValueP = (*iface)(unsafe.Pointer(&al.scratchValue))
 
-	var v LValue = LNumber(0)
-	vp := (*iface)(unsafe.Pointer(&v))
-	al.itabLNumber = vp.itab
 	return al
 }
 
+// LNumber2I takes a number value and returns an interface LValue representing the same number.
+// Converting an LNumber to a LValue naively, by doing:
+// `var val LValue = myLNumber`
+// will result in an individual heap alloc of 8 bytes for the float value. LNumber2I amortizes the cost and memory
+// overhead of these allocs by allocating blocks of floats instead.
+// The downside of this is that all of the floats on a given block have to become eligible for gc before the block
+// as a whole can be gc-ed.
 func (al *allocator) LNumber2I(v LNumber) LValue {
+	// first check for shared preloaded numbers
 	if v >= 0 && v < preloadLimit && float64(v) == float64(int64(v)) {
 		return preloads[int(v)]
 	}
-	if al.top == len(al.nptrs)-1 {
+
+	// check if we need a new alloc page
+	if al.top == len(al.fptrs)-1 {
 		al.top = 0
-		al.nptrs = make([]LValue, al.size)
-		al.nheader = (*reflect.SliceHeader)(unsafe.Pointer(&al.nptrs))
 		al.fptrs = make([]float64, al.size)
 		al.fheader = (*reflect.SliceHeader)(unsafe.Pointer(&al.fptrs))
 	}
-	fptr := (*float64)(unsafe.Pointer(al.fheader.Data + uintptr(al.top)*unsafe.Sizeof(_fv)))
-	e := *(*LValue)(unsafe.Pointer(al.nheader.Data + uintptr(al.top)*unsafe.Sizeof(_uv)))
-	al.top++
 
-	ep := (*iface)(unsafe.Pointer(&e))
-	ep.itab = al.itabLNumber
+	// alloc a new float, and store our value into it
+	fptr := (*float64)(unsafe.Pointer(al.fheader.Data + uintptr(al.top)*unsafe.Sizeof(_fv)))
+	al.top++
 	*fptr = float64(v)
-	ep.word = unsafe.Pointer(fptr)
-	return e
+
+	// hack our scratch LValue to point to our allocated value
+	// this scratch lvalue is copied when this function returns meaning the scratch value can be reused
+	// on the next call
+	al.scratchValueP.word = unsafe.Pointer(fptr)
+
+	return al.scratchValue
 }
