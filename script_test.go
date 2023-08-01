f189fe137dd2286c76789b88db03e080332fd0e6
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-08-01 14:06:09 -0400

    spelling: user
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- script_test.go
+++ script_test.go
@@ -110,7 +110,7 @@ func countFinalizers(L *LState) int {
 	return 1
 }
 
-// TestLocalVarFree verifies that tables and user user data which are no longer referenced by the lua script are
+// TestLocalVarFree verifies that tables and user data which are no longer referenced by the lua script are
 // correctly gc-ed. There was a bug in gopher lua where local vars were not being gc-ed in all circumstances.
 func TestLocalVarFree(t *testing.T) {
 	s := `
